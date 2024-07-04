376483b4f40a76b78638636e18206121b926af89
jc-development <joe.cardella3@gmail.com>
2024-07-04 05:20:39 -0400

    changed to onedark theme

--- init.lua
+++ init.lua
@@ -868,27 +868,39 @@ require('lazy').setup({
           { name = 'path' },
         },
         window = {
-          completion = { border = 'rounded' },
-          documentation = { border = 'rounded' },
+          completion = {
+            border = 'rounded',
+          },
+          documentation = {
+            border = 'rounded',
+          },
         },
       }
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'projekt0n/github-nvim-theme',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'github_dark_default'
+  --{ -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  --
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --  'projekt0n/github-nvim-theme',
+  --priority = 1000, -- Make sure to load this before all the other start plugins.
+  --init = function()
+  -- Load the colorscheme here.
+  -- Like many other themes, this one has different styles, and you could load
+  -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --vim.cmd.colorscheme 'github_dark_default'
 
-      -- You can configure highlights by doing something like:
+  -- You can configure highlights by doing something like:
+  --vim.cmd.hi 'Comment gui=none'
+  -- end,
+  --},
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000,
+    init = function()
+      vim.cmd.colorscheme 'onedark_dark'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
